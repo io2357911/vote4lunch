7261ee8f45619065bd62a26ad302545ddfc3d81c
Oleg Ivanov <io2357911@yandex.ru>
2020-10-28 21:02:44 +0300

    Add logging

--- src/main/java/com/github/io2357911/vote4lunch/Main.java
+++ src/main/java/com/github/io2357911/vote4lunch/Main.java
@@ -1,7 +1,12 @@
 package com.github.io2357911.vote4lunch;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        log.info("Hello world!");
     }
 }
