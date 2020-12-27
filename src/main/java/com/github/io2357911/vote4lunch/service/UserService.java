4b03c165ee53443032107c8b166f426082c71cf1
Oleg Ivanov <io2357911@yandex.ru>
2020-12-27 13:28:13 +0300

    Add Spring Security auth provider

---
+++ src/main/java/com/github/io2357911/vote4lunch/service/UserService.java
@@ -0,0 +1,27 @@
+package com.github.io2357911.vote4lunch.service;
+
+import com.github.io2357911.vote4lunch.AuthorizedUser;
+import com.github.io2357911.vote4lunch.model.User;
+import com.github.io2357911.vote4lunch.repository.UserJpaRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class UserService implements UserDetailsService {
+
+    private final UserJpaRepository repository;
+
+    public UserService(UserJpaRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public AuthorizedUser loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = repository.getByEmail(email.toLowerCase());
+        if (user == null) {
+            throw new UsernameNotFoundException("User " + email + " is not found");
+        }
+        return new AuthorizedUser(user);
+    }
+}
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,8 +9,8 @@ FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password, role)
-VALUES ('User', 'user@yandex.ru', 'password', 'USER'),
-       ('Admin', 'admin@gmail.com', 'admin', 'ADMIN');
+VALUES ('User', 'user@mail.com', '{noop}user', 'USER'),
+       ('Admin', 'admin@mail.com', '{noop}admin', 'ADMIN');
 
 INSERT INTO restaurants (name)
 VALUES ('McDonald''s'),
