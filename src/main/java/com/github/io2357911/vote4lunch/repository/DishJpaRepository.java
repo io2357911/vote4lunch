1a1e162f75cb55e9efb932b2bcc966eb1d481c16
Oleg Ivanov <io2357911@yandex.ru>
2020-12-31 11:39:39 +0300

    Remove unused repository methods

--- src/main/java/com/github/io2357911/vote4lunch/repository/DishJpaRepository.java
+++ src/main/java/com/github/io2357911/vote4lunch/repository/DishJpaRepository.java
@@ -2,7 +2,6 @@ package com.github.io2357911.vote4lunch.repository;
 
 import com.github.io2357911.vote4lunch.model.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,11 +11,6 @@ import java.util.List;
 
 @Transactional(readOnly = true)
 public interface DishJpaRepository extends JpaRepository<Dish, Integer> {
-    @Transactional
-    @Modifying
-    @Query("DELETE FROM Dish d WHERE d.id=:id AND d.restaurant.id=:restaurantId")
-    int delete(@Param("id") int id, @Param("restaurantId") int restaurantId);
-
     @Query("SELECT d FROM Dish d WHERE d.restaurant.id=:restaurantId AND d.date=:date")
     List<Dish> getAll(@Param("restaurantId") int restaurantId, @Param("date") LocalDate date);
 }
--- src/main/java/com/github/io2357911/vote4lunch/repository/RestaurantJpaRepository.java
+++ src/main/java/com/github/io2357911/vote4lunch/repository/RestaurantJpaRepository.java
@@ -3,7 +3,6 @@ package com.github.io2357911.vote4lunch.repository;
 import com.github.io2357911.vote4lunch.model.Restaurant;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +12,6 @@ import java.util.List;
 
 @Transactional(readOnly = true)
 public interface RestaurantJpaRepository extends JpaRepository<Restaurant, Integer> {
-    @Transactional
-    @Modifying
-    @Query("DELETE FROM Restaurant r WHERE r.id=:id")
-    int delete(@Param("id") int id);
-
     @EntityGraph(attributePaths = {"dishes"}, type = EntityGraph.EntityGraphType.LOAD)
     @Query("SELECT r FROM Restaurant r JOIN FETCH r.dishes d WHERE d.date = :date")
     List<Restaurant> getWithDishes(@Param("date") LocalDate date);
